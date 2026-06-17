3282fe7c325889a2c5b439be35244e0bc26c8c0f
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-06-17 15:01:23 -0700

    refactor: assets

--- EatFit/app/(auth)/auth.tsx
+++ EatFit/app/(auth)/auth.tsx
@@ -1,12 +1,16 @@
-// app/auth.tsx
+import { Redirect } from "expo-router";
 import Screen from "@shared/layout/Screen";
 import { AuthContent } from "@features/auth/components/AuthContent";
 import { colors } from "@shared/theme/colors";
-
-//imports 
-
+import { useAuth } from "@features/auth/useAuth";
 
 export default function AuthPage() {
+  const { session } = useAuth();
+
+  if (session) {
+    return <Redirect href="/home" />;
+  }
+
   return (
     <Screen scroll padded={false} backgroundColor={colors.background}>
       <AuthContent />
